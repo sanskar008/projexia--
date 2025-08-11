3818e745ea2620886f6d85cb5f8265e6351b1699
Sanskar Koserwal <sanskark008@gmail.com>
2025-08-11 10:58:54 +0530

    feat: add additional allowed origin for CORS configuration

--- server/prod-server.cjs
+++ server/prod-server.cjs
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 8080;
 const allowedOrigins = [
   "http://localhost:5173",
   "https://projexia-eight.vercel.app",
+  "https://projexia.sanskarkoserwal.online",
 ];
 
 app.use(
